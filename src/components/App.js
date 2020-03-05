8ad4f150cbdaff7e6e9f2e7526cc72a1e7540bcd
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-03-05 18:52:52 -0500

    replace page contents with header

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {'Edit '}
-          <code>src/App.js</code>
-          {' and save to reload.'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Taylor Laekeman</h1>
       </header>
-    </div>
+    </>
   );
 }
 
