2268394e0735d1a626035db528cb727328aaa949
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-03-09 17:57:39 -0400

    fix font bug

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,600&display=swap');
 
   body {
     background-color: ${props => props.theme.colours.background};
