034f279b3fbedd99742eda7396445ad86a84222f
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-03-09 20:38:24 -0400

    remove description

--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,9 @@ export default () => {
       <header>
         <h1>Taylor Laekeman</h1>
       </header>
-      <Event description="test" title="Today" />
+      <Event title="Today" />
       <Event
         date="August 20, 2018"
-        description="test"
         title="Joined Canvass Analytics as a Full-stack Web Developer"
       />
     </ThemeProvider>
