726e2336269fb8be908f88afc297541b412a9968
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-03-11 19:52:49 -0400

    add timeline design to page

--- src/components/Event.js
+++ src/components/Event.js
@@ -8,14 +8,37 @@ const Article = styled.article`
 
 const Header = styled.header`
   display: grid;
-  grid-gap: 6px;
+  grid-template-areas:
+    '.    date '
+    'icon title';
+  grid-template-columns: 13px 1fr;
+  grid-gap: 6px 18px;
+
+  &:after {
+    content: '';
+    grid-area: icon;
+    background-color: white;
+    border: solid ${props => props.theme.colours.timeline} 2px;
+    border-radius: 50%;
+    height: 13px;
+    z-index: 2;
+    box-sizing: border-box;
+  }
+`;
+
+const Title = styled.h2`
+  grid-area: title;
+`;
+
+const Time = styled.time`
+  grid-area: date;
 `;
 
 const Event = ({ date, description, title }) => (
   <Article>
     <Header>
-      {date && <time>{date}</time>}
-      <h2>{title}</h2>
+      {date && <Time>{date}</Time>}
+      <Title>{title}</Title>
     </Header>
     {description && (
       <main>
