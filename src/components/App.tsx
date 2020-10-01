e3d8c948580306926141fbd2f2269661fd478556
taylor <taylor.laekeman@gmail.com>
2020-09-30 22:31:23 -0400

    fix lint errors in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 
@@ -9,6 +9,50 @@ import Link from 'components/Link';
 import Resume from 'components/Resume';
 import Global from 'styles/Global';
 import theme from 'styles/theme';
+
+const App: FunctionComponent = () => (
+  <ThemeProvider theme={theme}>
+    <Global />
+    <Article>
+      <Header>
+        <h1>Taylor Laekeman</h1>
+        <a href="mailto:hello@tjlaekem.dev">
+          <Envelope />
+        </a>
+        <a href="https://github.com/tjlaekem">
+          <Github />
+        </a>
+      </Header>
+      <Router>
+        <Switch>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer>
+        <List>
+          <Item>
+            <Link href="mailto:hello@tjlaekem.dev">Email</Link>
+          </Item>
+          <Item>
+            <Link href="https://github.com/tjlaekem">GitHub</Link>
+          </Item>
+          <Item>
+            <Link href="https://www.linkedin.com/in/tjlaekem/">LinkedIn</Link>
+          </Item>
+        </List>
+        <Copyright>
+          &copy;
+          {` ${new Date(Date.now()).getFullYear()} Taylor Laekeman`}
+        </Copyright>
+      </Footer>
+    </Article>
+  </ThemeProvider>
+);
 
 const Article = styled.article`
   display: grid;
@@ -63,48 +107,4 @@ const Copyright = styled.p`
   margin: 24px 0;
 `;
 
-export default () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Global />
-      <Article>
-        <Header>
-          <h1>Taylor Laekeman</h1>
-          <a href="mailto:hello@tjlaekem.dev">
-            <Envelope />
-          </a>
-          <a href="https://github.com/tjlaekem">
-            <Github />
-          </a>
-        </Header>
-        <Router>
-          <Switch>
-            <Route path="/resume">
-              <Resume />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Router>
-        <Footer>
-          <List>
-            <Item>
-              <Link href="mailto:hello@tjlaekem.dev">Email</Link>
-            </Item>
-            <Item>
-              <Link href="https://github.com/tjlaekem">GitHub</Link>
-            </Item>
-            <Item>
-              <Link href="https://www.linkedin.com/in/tjlaekem/">LinkedIn</Link>
-            </Item>
-          </List>
-          <Copyright>
-            &copy;
-            {` ${new Date(Date.now()).getFullYear()} Taylor Laekeman`}
-          </Copyright>
-        </Footer>
-      </Article>
-    </ThemeProvider>
-  );
-};
+export default App;
