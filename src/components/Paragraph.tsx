2635a57ba800d02c4c5482e4850aa64cc9a70543
taylor <taylor.laekeman@gmail.com>
2020-10-03 16:46:44 -0400

    style paragraph

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,5 +1,34 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
 
-const Paragraph = () => (<div>paragraph</div>);
+const Paragraph: FunctionComponent<propTypes> = ({ children }: propTypes) => (
+  <Text>{children}</Text>
+);
+
+export type propTypes = {
+  children: string;
+};
+
+const Text = styled.p`
+  color: ${({ theme }) => theme.colours.text};
+  font-family: ${({ theme }) => theme.font};
+  font-size: 0.9rem;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+
+  @media (min-width: 400px) {
+    max-width: 400px;
+  }
+
+  @media (min-width: 800px) {
+    font-size: 1rem;
+  }
+
+  @media (min-width: 1400px) {
+    font-size: 1.2rem;
+    max-width: 600px;
+  }
+`;
 
 export default Paragraph;
