7f415ad072add17631a5c1fc222b2f0dcda3909a
taylor <taylor.laekeman@gmail.com>
2020-10-03 13:36:49 -0400

    add normal nav item story

---
+++ src/stories/NavItem.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import NavItem, { propTypes } from 'components/NavItem';
+
+export default {
+  title: 'NavItem',
+  component: NavItem,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<propTypes> = (args) => <NavItem {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+  children: 'Projects',
+  isCurrent: false,
+};
