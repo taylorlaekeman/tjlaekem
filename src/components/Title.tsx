f561e84a09394d6aa1ff50a95f70c0e867d463cc
taylor <taylor.laekeman@gmail.com>
2020-10-03 16:29:26 -0400

    add responsive sizing for title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -12,10 +12,18 @@ export type propTypes = {
 const Text = styled.h2`
   color: ${({ theme }) => theme.colours.text};
   font-family: ${({ theme }) => theme.font};
-  font-size: 5rem;
+  font-size: 2rem;
   font-weight: 600;
   margin: 0;
   padding: 0;
+
+  @media (min-width: 800px) {
+    font-size: 3rem;
+  }
+
+  @media (min-width: 1400px) {
+    font-size: 5rem;
+  }
 `;
 
 export default Title;
