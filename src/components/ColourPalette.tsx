114406f5e1d6c9b4b5ec373f166d026a5b31231e
taylor <taylor.laekeman@gmail.com>
2020-10-02 22:51:22 -0400

    finish colour palette component

--- src/components/ColourPalette.tsx
+++ src/components/ColourPalette.tsx
@@ -6,6 +6,25 @@ const ColourPalette: FunctionComponent = ({ palette }: propTypes) => (
     {SHADES.map((shade) => (
       <ColourTile $palette={palette} $shade={shade} />
     ))}
+    {SHADES.map((shade) => (
+      <TextTile
+        $backgroundPalette="white"
+        $textPalette={palette}
+        $textShade={shade}
+      >
+        Taylor Laekeman
+      </TextTile>
+    ))}
+    {SHADES.map((backgroundShade) => SHADES.map((textShade) => (
+      <TextTile
+        $backgroundPalette={palette}
+        $backgroundShade={backgroundShade}
+        $textPalette={palette}
+        $textShade={textShade}
+      >
+        Taylor Laekeman
+      </TextTile>
+    )))}
   </Wrapper>
 );
 
@@ -17,18 +36,22 @@ const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(9, 1fr);
   grid-template-rows: 10fr repeat(10, 1fr);;
-  height: 500px;
+  height: 600px;
   width: 100%;
 `;
 
 const ColourTile = styled.div<{ $palette: string; $shade: string; }>`
-  background-color: green;
-  ${({ theme, $palette, $shade }) => {
-    console.log({ theme, $palette, $shade });
-    console.log(theme.colours);
-  }}
+  background-color: ${({ $palette, $shade, theme }) => theme.colours[$palette][$shade]};
   height: 100%;
   width: 100%;
+
+  &:first-child {
+    border-radius: 10px 0 0 0;
+  }
+
+  &:nth-child(9) {
+    border-radius: 0 10px 0 0;
+  }
 `;
 
 const SHADES = [
@@ -43,7 +66,20 @@ const SHADES = [
   '900',
 ];
 
-const TextTile = styled.div`
+const TextTile = styled.div<{ $backgroundShade: string; $palette: string; $textShade: string; }>`
+  align-items: center;
+  background-color: ${({ $backgroundPalette, $backgroundShade, theme }) => $backgroundShade ? theme.colours[$backgroundPalette][$backgroundShade] : theme.colours[$backgroundPalette]};
+  color: ${({ $textPalette, $textShade, theme }) => theme.colours[$textPalette][$textShade]};
+  display: flex;
+  justify-content: center;
+
+  &:nth-child(91) {
+    border-radius: 0 0 0 10px;
+  }
+
+  &:last-child {
+    border-radius: 0 0 10px 0;
+  }
 `;
 
 export default ColourPalette;
