6cfb26476bfbfc37e95459b8c95c2debc2c378ec
taylor <taylor.laekeman@gmail.com>
2020-10-04 15:11:01 -0400

    add nav element to nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,13 @@
 import React, { FunctionComponent, ReactNode } from 'react';
-import styled from 'styled-components';
 
 const Nav: FunctionComponent<propTypes> = ({ children }: propTypes) => (
-  <Wrapper>{children}</Wrapper>
+  <nav>
+    <ul>{children}</ul>
+  </nav>
 );
 
 export type propTypes = {
   children: ReactNode;
 };
-
-const Wrapper = styled.ul``;
 
 export default Nav;
