c5866f936af47a1b997f39a6c66e0a63afd8d5ad
taylor <taylor.laekeman@gmail.com>
2020-10-04 17:44:39 -0400

    center homepage and add canvass experience

--- src/components/Tjlaekem.tsx
+++ src/components/Tjlaekem.tsx
@@ -2,14 +2,49 @@ import React, { FunctionComponent } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 
+import Category from 'components/Category';
+import CategoryTitle from 'components/CategoryTitle';
 import Header from 'components/Header';
 import Home from 'components/Home';
+import Image from 'components/Image';
+import Links from 'components/Links';
 import Link from 'components/Link';
+import NewLink from 'components/NewLink';
+import Paragraph from 'components/Paragraph';
 import Resume from 'components/Resume';
+import Section from 'components/Section';
+import SectionTitle from 'components/SectionTitle';
 
 const App: FunctionComponent = () => (
-  <>
+  <Wrapper>
     <Header>Taylor Laekeman</Header>
+    <main>
+      <Category>
+        <CategoryTitle>Experience</CategoryTitle>
+        <Section>
+          <SectionTitle area="title">Canvass Analytics</SectionTitle>
+          <Paragraph area="description">
+            At Canvass Analytics, we build tools that allow industrial engineers
+            to train machine learning models on their process data and deploy
+            them for real-time predictions. As a Fullstack Software Engineer at
+            Canvass, I work to identify problems, design solutions, and
+            implement functionality across the stack. Most of my work focuses on
+            our front-end React app, our Kafka-based ingestion pipeline, and our
+            Kubeflow-orchestrated training pipeline.
+          </Paragraph>
+          <Image
+            alt="The Canvass Analytics logo.  A geometric 'C' in a light-blue circle on a white background."
+            area="image"
+            src="https://static.wixstatic.com/media/0a64fb_d389afd95f6544978bb2fd978f614ac4~mv2.png/v1/fill/w_519,h_519,al_c,lg_1,q_85/0a64fb_d389afd95f6544978bb2fd978f614ac4~mv2.webp"
+          />
+          <Links>
+            <NewLink to="https://www.canvass.io">
+              read more about Canvass
+            </NewLink>
+          </Links>
+        </Section>
+      </Category>
+    </main>
     <Article>
       <Router>
         <Switch>
@@ -39,8 +74,14 @@ const App: FunctionComponent = () => (
         </Copyright>
       </Footer>
     </Article>
-  </>
+  </Wrapper>
 );
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
 
 const Article = styled.article`
   display: grid;
