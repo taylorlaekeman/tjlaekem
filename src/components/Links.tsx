a9defb12e45dd86477f533e23e46e6c1f76c3e5e
taylor <taylor.laekeman@gmail.com>
2020-10-04 15:26:55 -0400

    add links component

---
+++ src/components/Links.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent, ReactNode } from 'react';
+import styled from 'styled-components';
+
+const Links: FunctionComponent<propTypes> = ({ children }: propTypes) => (
+  <Wrapper>{children}</Wrapper>
+);
+
+export type propTypes = {
+  children: ReactNode,
+};
+
+const Wrapper = styled.nav`
+  display: flex;
+  flex-direction: column;
+
+  a {
+    margin-bottom: 20px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export default Links;
