bb5e5b83409016f37dd98fbc95bde7fd8b229dce
taylor <taylor.laekeman@gmail.com>
2020-10-04 17:23:48 -0400

    use new header

--- src/components/Tjlaekem.tsx
+++ src/components/Tjlaekem.tsx
@@ -2,51 +2,44 @@ import React, { FunctionComponent } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { ReactComponent as UnstyledEnvelope } from 'assets/envelope.svg';
-import { ReactComponent as UnstyledGithub } from 'assets/github.svg';
+import Header from 'components/Header';
 import Home from 'components/Home';
 import Link from 'components/Link';
 import Resume from 'components/Resume';
 
 const App: FunctionComponent = () => (
-  <Article>
-    <Header>
-      <h1>Taylor Laekeman</h1>
-      <a href="mailto:hello@tjlaekem.dev">
-        <Envelope />
-      </a>
-      <a href="https://github.com/tjlaekem">
-        <Github />
-      </a>
-    </Header>
-    <Router>
-      <Switch>
-        <Route path="/resume">
-          <Resume />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-    <Footer>
-      <List>
-        <Item>
-          <Link href="mailto:hello@tjlaekem.dev">Email</Link>
-        </Item>
-        <Item>
-          <Link href="https://github.com/tjlaekem">GitHub</Link>
-        </Item>
-        <Item>
-          <Link href="https://www.linkedin.com/in/tjlaekem/">LinkedIn</Link>
-        </Item>
-      </List>
-      <Copyright>
-        &copy;
-        {` ${new Date(Date.now()).getFullYear()} Taylor Laekeman`}
-      </Copyright>
-    </Footer>
-  </Article>
+  <>
+    <Header>Taylor Laekeman</Header>
+    <Article>
+      <Router>
+        <Switch>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer>
+        <List>
+          <Item>
+            <Link href="mailto:hello@tjlaekem.dev">Email</Link>
+          </Item>
+          <Item>
+            <Link href="https://github.com/tjlaekem">GitHub</Link>
+          </Item>
+          <Item>
+            <Link href="https://www.linkedin.com/in/tjlaekem/">LinkedIn</Link>
+          </Item>
+        </List>
+        <Copyright>
+          &copy;
+          {` ${new Date(Date.now()).getFullYear()} Taylor Laekeman`}
+        </Copyright>
+      </Footer>
+    </Article>
+  </>
 );
 
 const Article = styled.article`
@@ -55,30 +48,6 @@ const Article = styled.article`
     'header '
     'content'
     'footer ';
-`;
-
-const Header = styled.header`
-  grid-area: header;
-  display: grid;
-  grid-template-areas: 'name envelope github';
-  grid-template-columns: 1fr min-content min-content;
-  grid-gap: 12px;
-  align-items: center;
-  ${(props) => props.theme.responsivePadding}
-`;
-
-const Envelope = styled(UnstyledEnvelope)`
-  grid-area: envelope;
-  fill: ${(props) => props.theme.colours.icon};
-  height: 20px;
-  width: 20px;
-`;
-
-const Github = styled(UnstyledGithub)`
-  grid-area: github;
-  fill: ${(props) => props.theme.colours.icon};
-  height: 24px;
-  width: 24px;
 `;
 
 const Footer = styled.footer`
