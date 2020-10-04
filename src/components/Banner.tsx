e3f0e216c67be659bb00b69bce824bff59f7a8ea
taylor <taylor.laekeman@gmail.com>
2020-10-04 17:09:48 -0400

    add banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,17 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
 
 const Banner: FunctionComponent<propTypes> = ({ children }: propTypes) => (
-  <div>banner</div>
+  <Text>{children}</Text>
 );
 
 export type propTypes = {
   children: string;
 };
+
+const Text = styled.p`
+  font-size: 7rem;
+  font-weight: 500;
+`;
 
 export default Banner;
