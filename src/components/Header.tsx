06ce169a27f6c89e59e0d30e76d952aa06671abe
taylor <taylor.laekeman@gmail.com>
2020-10-04 16:43:52 -0400

    add header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,49 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as Github } from 'assets/github.svg';
+import { ReactComponent as Linkedin } from 'assets/linkedin.svg';
 
 const Header: FunctionComponent<propTypes> = ({ children }: propTypes) => (
-  <div>header</div>
+  <Wrapper>
+    <h1>{children}</h1>
+    <Socials>
+      <IconLink href="https://github.com/tjlaekem"><Github /></IconLink>
+      <IconLink href="https://linkedin.com/in/tjlaekem"><Linkedin /></IconLink>
+    </Socials>
+  </Wrapper>
 );
 
 export type propTypes = {
   children: string;
 };
+
+const Wrapper = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Socials = styled.nav`
+  display: flex;
+
+  & > a {
+    margin-right: 40px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+`;
+
+const IconLink = styled.a`
+  display: block;
+  fill: ${({ theme }) => theme.colours.neutral[600]};
+  width: 24px;
+
+  &:hover {
+    fill: ${({ theme }) => theme.colours.neutral[900]};
+  }
+`;
 
 export default Header;
