d1f0fbc79d3faaf03f187acf85f81619ea22edac
taylor <taylor.laekeman@gmail.com>
2020-10-05 22:21:44 -0400

    separate default and project section types

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,33 +1,65 @@
 import React, { FunctionComponent, ReactNode } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Section: FunctionComponent<propTypes> = ({
   children,
   isFlipped = false,
-}: propTypes) => <Wrapper $isFlipped={isFlipped}>{children}</Wrapper>;
+  type = 'default',
+}: propTypes) => (
+  <Wrapper $isFlipped={isFlipped} $type={type}>
+    {children}
+  </Wrapper>
+);
 
 export type propTypes = {
   children: ReactNode;
   isFlipped?: boolean;
+  type?: 'project' | 'default';
 };
 
-const Wrapper = styled.section<{ $isFlipped: boolean }>`
+const Wrapper = styled.section<{ $isFlipped: boolean; $type: string }>`
+  ${({ $isFlipped, $type }) => getGrid($type, $isFlipped)};
+  max-width: 800px;
+`;
+
+const getGrid = (type: string, isFlipped: boolean) => {
+  if (isFlipped) {
+    switch (type) {
+      case 'project':
+        return FLIPPED_PROJECT_GRID;
+      default:
+        return null;
+    }
+  } else {
+    switch (type) {
+      case 'project':
+        return PROJECT_GRID;
+      default:
+        return null;
+    }
+  }
+};
+
+const SHARED = css`
   display: grid;
   grid-gap: 40px 60px;
-  grid-template-areas: ${({ $isFlipped }) =>
-    $isFlipped
-      ? `
+  grid-template-columns: 1fr 1fr;
+`;
+
+const FLIPPED_PROJECT_GRID = css`
+  ${SHARED}
+  grid-template-areas:
     'image title      '
     'image description'
     'image links      ';
-  `
-      : `
+`;
+
+const PROJECT_GRID = css`
+  ${SHARED}
+  grid-template-areas:
     'title       image'
     'description image'
     'links       image';
-  `}
-  grid-template-columns: 1fr 1fr;
-  max-width: 800px;
 `;
 
 export default Section;
