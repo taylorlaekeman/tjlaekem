14b5b0efbe606ecf599fd20e3f08cd26ee2c726c
taylor <taylor.laekeman@gmail.com>
2020-10-10 16:44:50 -0400

    remove unused styled import from section component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent, ReactNode } from 'react';
-import styled from 'styled-components';
 
 const Section: FunctionComponent<propTypes> = ({ children }: propTypes) => (
   <section>{children}</section>
