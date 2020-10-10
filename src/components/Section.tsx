c5b7f30a0b379cb4cf4a54e829a2fb4e8cbfd34a
taylor <taylor.laekeman@gmail.com>
2020-10-09 23:10:12 -0400

    fix type and lint errors in section and section story

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,22 +1,15 @@
 import React, { FunctionComponent, ReactNode } from 'react';
 import styled from 'styled-components';
 
-const Section: FunctionComponent<propTypes> = ({
-  children,
-  isFlipped = false,
-  type = 'default',
-}: propTypes) => (
-  <Wrapper $isFlipped={isFlipped} $type={type}>
-    {children}
-  </Wrapper>
+const Section: FunctionComponent<propTypes> = ({ children }: propTypes) => (
+  <Wrapper>{children}</Wrapper>
 );
 
 export type propTypes = {
   children: ReactNode;
-  isFlipped?: boolean;
 };
 
-const Wrapper = styled.section<{ $isFlipped: boolean; $type: string }>`
+const Wrapper = styled.section`
   display: grid;
   grid-gap: 40px;
 
