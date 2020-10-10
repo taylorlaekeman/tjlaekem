207fa8856c1f9f2043ac594b9c59a8e4b1c1894b
taylor <taylor.laekeman@gmail.com>
2020-10-10 12:26:05 -0400

    add section title type to text component

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,29 +1,53 @@
 import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Text: FunctionComponent<propTypes> = ({
   area = '',
   children,
   type = 'paragraph',
 }: propTypes) => {
-  return (
-    <Paragraph area={area}>{children}</Paragraph>
-  );
+  const Component = getComponent(type);
+  return <Component $area={area}>{children}</Component>;
 };
 
 export type propTypes = {
   area?: string;
   children: string;
-  type?: 'paragraph' | 'section-title' | 'category-title' | 'banner';
+  type?: validTypes;
 };
 
-const Paragraph = styled.p<{ $area: string }>`
+type validTypes = 'paragraph' | 'section-title' | 'category-title' | 'banner';
+
+const getComponent = (type: validTypes) => {
+  switch (type) {
+    case 'section-title':
+      return SectionTitle;
+    default:
+      return Paragraph;
+  }
+};
+
+const sharedStyles = css<componentPropTypes>`
   color: ${({ theme }) => theme.colours.text};
+  grid-area: ${({ $area }) => $area};
+`;
+
+const SectionTitle = styled.h2<componentPropTypes>`
+  font-size: 3rem;
+  font-weight: 600;
+  ${sharedStyles}
+`;
+
+const Paragraph = styled.p<componentPropTypes>`
   font-size: 1.2rem;
   font-weight: 400;
-  grid-area: ${({ $area }) => $area};
   line-height: 24px;
   max-width: 600px;
+  ${sharedStyles}
 `;
+
+type componentPropTypes = {
+  $area: string;
+};
 
 export default Text;
