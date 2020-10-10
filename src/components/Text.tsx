214922cc76d6afc48d3ce6a7daeb815b3dcfb597
taylor <taylor.laekeman@gmail.com>
2020-10-10 12:10:50 -0400

    add paragraph type to text component

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,11 +1,29 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
 
-const Text: FunctionComponent<propTypes> = ({ children }: propTypes) => (
-  <div>text</div>
-);
+const Text: FunctionComponent<propTypes> = ({
+  area = '',
+  children,
+  type = 'paragraph',
+}: propTypes) => {
+  return (
+    <Paragraph area={area}>{children}</Paragraph>
+  );
+};
 
 export type propTypes = {
+  area?: string;
   children: string;
+  type?: 'paragraph' | 'section-title' | 'category-title' | 'banner';
 };
+
+const Paragraph = styled.p<{ $area: string }>`
+  color: ${({ theme }) => theme.colours.text};
+  font-size: 1.2rem;
+  font-weight: 400;
+  grid-area: ${({ $area }) => $area};
+  line-height: 24px;
+  max-width: 600px;
+`;
 
 export default Text;
