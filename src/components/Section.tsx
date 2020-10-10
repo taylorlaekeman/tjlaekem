f6b94909382b4e9d76a0f3aa161339055783b738
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-10-09 23:13:09 -0400

    remove images
    * remove images from tjlaekem component
    
    * remove images from section stories
    
    * remove images and simplify section component
    
    * fix type and lint errors in section and section story

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,109 +1,21 @@
 import React, { FunctionComponent, ReactNode } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-const Section: FunctionComponent<propTypes> = ({
-  children,
-  isFlipped = false,
-  type = 'default',
-}: propTypes) => (
-  <Wrapper $isFlipped={isFlipped} $type={type}>
-    {children}
-  </Wrapper>
+const Section: FunctionComponent<propTypes> = ({ children }: propTypes) => (
+  <Wrapper>{children}</Wrapper>
 );
 
 export type propTypes = {
   children: ReactNode;
-  isFlipped?: boolean;
-  type?: 'default' | 'education' | 'experience' | 'project';
 };
 
-const Wrapper = styled.section<{ $isFlipped: boolean; $type: string }>`
-  ${({ $isFlipped, $type }) => getGrid($type, $isFlipped)};
+const Wrapper = styled.section`
+  display: grid;
+  grid-gap: 40px;
 
   @media (min-width: 800px) {
     width: 800px;
   }
-`;
-
-const getGrid = (type: string, isFlipped: boolean) => {
-  if (isFlipped) {
-    switch (type) {
-      case 'education':
-        return EDUCATION_GRID;
-      case 'experience':
-        return FLIPPED_EXPERIENCE_GRID;
-      case 'project':
-        return FLIPPED_PROJECT_GRID;
-      default:
-        return null;
-    }
-  } else {
-    switch (type) {
-      case 'education':
-        return EDUCATION_GRID;
-      case 'experience':
-        return EXPERIENCE_GRID;
-      case 'project':
-        return PROJECT_GRID;
-      default:
-        return null;
-    }
-  }
-};
-
-const SHARED = css`
-  display: grid;
-  grid-gap: 40px 60px;
-  grid-template-columns: 1fr 1fr;
-`;
-
-const FLIPPED_EXPERIENCE_GRID = css`
-  ${SHARED}
-  grid-template-areas:
-    'image title      '
-    'image role       '
-    'image description'
-    'image links      '
-    'image .          ';
-`;
-
-const FLIPPED_PROJECT_GRID = css`
-  ${SHARED}
-  grid-template-areas:
-    'image title      '
-    'image description'
-    'image links      '
-    'image .          ';
-`;
-
-const EDUCATION_GRID = css`
-  ${SHARED}
-  grid-gap: 40px;
-  grid-template-areas:
-    'title      '
-    'degree     '
-    'dates      '
-    'description';
-  grid-template-columns: 1fr;
-`;
-
-const EXPERIENCE_GRID = css`
-  ${SHARED}
-  grid-template-areas:
-    'title       image'
-    'role        image'
-    'description image'
-    'links       image'
-    '.           image';
-`;
-
-const PROJECT_GRID = css`
-  ${SHARED}
-  grid-template-areas:
-    'title       image'
-    'description image'
-    'links       image'
-    '.           image';
 `;
 
 export default Section;
--- src/stories/Category.stories.tsx
+++ src/stories/Category.stories.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 
-import RudderLogo from 'assets/rudder-logo.png';
 import Category, { propTypes } from 'components/Category';
 import CategoryTitle from 'components/CategoryTitle';
-import Image from 'components/Image';
 import Links from 'components/Links';
 import Link from 'components/NewLink';
 import Section from 'components/Section';
@@ -25,8 +23,8 @@ Normal.args = {
     <>
       <CategoryTitle>Projects</CategoryTitle>
       <Section>
-        <SectionTitle area="title">Title</SectionTitle>
-        <Paragraph area="description">
+        <SectionTitle>Title</SectionTitle>
+        <Paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -39,28 +37,10 @@ Normal.args = {
           <Link to="test">view the site</Link>
           <Link to="test">view the code</Link>
         </Links>
-        <Image alt="test" area="image" src={RudderLogo} />
-      </Section>
-      <Section isFlipped>
-        <SectionTitle area="title">Title</SectionTitle>
-        <Paragraph area="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Paragraph>
-        <Links>
-          <Link to="test">view the site</Link>
-          <Link to="test">view the code</Link>
-        </Links>
-        <Image alt="test" area="image" src={RudderLogo} />
       </Section>
       <Section>
-        <SectionTitle area="title">Title</SectionTitle>
-        <Paragraph area="description">
+        <SectionTitle>Title</SectionTitle>
+        <Paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -73,7 +53,22 @@ Normal.args = {
           <Link to="test">view the site</Link>
           <Link to="test">view the code</Link>
         </Links>
-        <Image alt="test" area="image" src={RudderLogo} />
+      </Section>
+      <Section>
+        <SectionTitle>Title</SectionTitle>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Paragraph>
+        <Links>
+          <Link to="test">view the site</Link>
+          <Link to="test">view the code</Link>
+        </Links>
       </Section>
     </>
   ),
