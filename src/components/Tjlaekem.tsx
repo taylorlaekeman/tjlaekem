d4d2cb477e0d5041486a6ca0129181e556d1fc74
taylor <taylor.laekeman@gmail.com>
2020-10-10 12:37:47 -0400

    replace all text with text component in tjlaekem

--- src/components/Tjlaekem.tsx
+++ src/components/Tjlaekem.tsx
@@ -10,6 +10,7 @@ import NewLink from 'components/NewLink';
 import Paragraph from 'components/Paragraph';
 import Section from 'components/Section';
 import SectionTitle from 'components/SectionTitle';
+import Text from 'components/Text';
 
 const App: FunctionComponent = () => (
   <Wrapper>
@@ -17,17 +18,17 @@ const App: FunctionComponent = () => (
     <main>
       <Category>
         <Section>
-          <Banner>Hi, I&apos;m Taylor</Banner>
+          <Text type="banner">Hi, I&apos;m Taylor</Text>
         </Section>
       </Category>
       <Category>
-        <CategoryTitle>Experience</CategoryTitle>
+        <Text type="category">Experience</Text>
         <Section>
-          <SectionTitle>Canvass Analytics</SectionTitle>
-          <Paragraph>
+          <Text type="section">Canvass Analytics</Text>
+          <Text type="paragraph">
             Full Stack Software Engineer, August 2018&ndash;Present
-          </Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">
             At Canvass Analytics, we build tools that allow industrial engineers
             to train machine learning models on their process data and deploy
             them for real-time predictions. As a Fullstack Software Engineer at
@@ -35,7 +36,7 @@ const App: FunctionComponent = () => (
             implement functionality across the stack. Most of my work focuses on
             our front-end React app, our Kafka-based ingestion pipeline, and our
             Kubeflow-orchestrated training pipeline.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://www.canvass.io">
               read more about Canvass
@@ -43,11 +44,11 @@ const App: FunctionComponent = () => (
           </Links>
         </Section>
         <Section>
-          <SectionTitle>Fourwalls Digital</SectionTitle>
-          <Paragraph>
+          <Text type="section">Fourwalls Digital</Text>
+          <Text type="paragraph">
             Software Development Coop, September 2017&ndash;December 2017
-          </Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">
             Fourwalls Digital is a tool built for real estate agents and
             brokerages in Toronto. During my coop with Fourwalls, I participated
             in the design and implementation of an extension that allows
@@ -57,7 +58,7 @@ const App: FunctionComponent = () => (
             platform to allow users to host exclusive real estate listings, and
             extend existing listings with additional information, photographs,
             and videos.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://fourwallsdigital.com/">
               read more about Fourwalls
@@ -65,11 +66,11 @@ const App: FunctionComponent = () => (
           </Links>
         </Section>
         <Section>
-          <SectionTitle>AbacusNext</SectionTitle>
-          <Paragraph>
+          <Text type="section">AbacusNext</Text>
+          <Text type="paragraph">
             Software Development Coop, September 2016&ndash;December 2016
-          </Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">
             AbacusNext offers a range of digital tools for lawyers and law
             firms. At AbacusNext, I designed and implemented an intermediate API
             that standardizes the interface to their various API servers for
@@ -77,7 +78,7 @@ const App: FunctionComponent = () => (
             redesigns that would have been needed to synchronize them. I also
             designed and implemented a replacement for AbacusNext&apos;s
             imperfect and outdated server monitor
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://www.abacusnext.com/">
               read more about AbacusNext
@@ -85,17 +86,17 @@ const App: FunctionComponent = () => (
           </Links>
         </Section>
         <Section>
-          <SectionTitle>Encircle</SectionTitle>
-          <Paragraph>
+          <Text type="section">Encircle</Text>
+          <Text type="paragraph">
             Software Development Coop, January 2016&ndash;April 2016
-          </Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">
             Encircle is building a platform for stakeholders at every step of
             the insurance process. While at Encircle, I contributed to the
             development of the Android and web applications. I redesigned the
             platform’s header and main navigation system and implemented the
             design after it was approved.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://www.getencircle.com/">
               read more about Encircle
@@ -103,11 +104,11 @@ const App: FunctionComponent = () => (
           </Links>
         </Section>
         <Section>
-          <SectionTitle>MetricWire</SectionTitle>
-          <Paragraph>
+          <Text type="section">MetricWire</Text>
+          <Text type="paragraph">
             Software Development Coop, May 2015&ndash;August 2015
-          </Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">
             MetricWire is a survey-delivery tool for mobile phones. At
             MetricWire I worked on the company&apos;s Android application. I
             designed and implemented survey functionality that allows the
@@ -117,7 +118,7 @@ const App: FunctionComponent = () => (
             coordinated visual design with the iOS development team in order to
             eliminate discrepancies across the platform, any of which could
             introduce platform bias into survey results.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://metricwire.com/">
               read more about MetricWire
@@ -125,11 +126,11 @@ const App: FunctionComponent = () => (
           </Links>
         </Section>
         <Section>
-          <SectionTitle>CBTS Canada Inc</SectionTitle>
-          <Paragraph>
+          <Text type="section">CBTS Canada Inc</Text>
+          <Text type="paragraph">
             Software Development Coop, September 2014&ndash;December 2014
-          </Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">
             CBTS Canada was an experimental project setup to investigate
             automated workstation management to supplement the IT organization
             at CBTS. Taking a leadership role on a team of coop students, I
@@ -139,23 +140,23 @@ const App: FunctionComponent = () => (
             organized modules into larger catalogues to represent the complete
             target configuration of a specific workstation, or class of
             workstations.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://www.cbts.com/">read more about CBTS</NewLink>
           </Links>
         </Section>
         <Section>
-          <SectionTitle>Cyclica</SectionTitle>
-          <Paragraph>
+          <Text type="section">Cyclica</Text>
+          <Text type="paragraph">
             Software Development Coop, January 2014&ndash;April 2014
-          </Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">
             Cyclica offers an drug-discovery platform for pharmaceutical
             researchers. At Cyclica, I implemented a program that converts
             pharmaceuticals into a graph representation, and compares their
             geometries to the known geometries of proteins in the human body in
             order to identify potential side-effects.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://www.cyclicarx.com/">
               read more about Cyclica
@@ -163,15 +164,15 @@ const App: FunctionComponent = () => (
           </Links>
         </Section>
         <Section>
-          <SectionTitle>Agriculture and Agri-Food Canada</SectionTitle>
-          <Paragraph>
+          <Text type="section">Agriculture and Agri-Food Canada</Text>
+          <Text type="paragraph">
             Software Development Coop, January 2013&ndash;April 2013
-          </Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">
             At Agriculture and Agri-Food Canada, I created a web portal for the
             Canadian government&apos;s newly digitized biological information
             archive.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://www.agr.gc.ca/eng/agriculture-and-agri-food-canada">
               read more about AAFC
@@ -180,14 +181,14 @@ const App: FunctionComponent = () => (
         </Section>
       </Category>
       <Category>
-        <CategoryTitle>Projects</CategoryTitle>
+        <Text type="category">Projects</Text>
         <Section>
-          <SectionTitle>Ciao, Estrela Co.</SectionTitle>
-          <Paragraph>
+          <Text type="section">Ciao, Estrela Co.</Text>
+          <Text type="paragraph">
             Rudder is a simple and opinionated task management tool designed
             according to the principles that underpin agile software
             development.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://cioaestrela.co">ciaoestrela.co</NewLink>
             <NewLink to="https://github.com/tjlaekem?tab=repositories&q=ciaoestrela&type=&language=">
@@ -196,12 +197,12 @@ const App: FunctionComponent = () => (
           </Links>
         </Section>
         <Section>
-          <SectionTitle>Rudder</SectionTitle>
-          <Paragraph>
+          <Text type="section">Rudder</Text>
+          <Text type="paragraph">
             Rudder is a simple and opinionated task management tool designed
             according to the principles that underpin agile software
             development.
-          </Paragraph>
+          </Text>
           <Links>
             <NewLink to="https://rudder.to">rudder.to</NewLink>
             <NewLink to="https://github.com/tjlaekem?tab=repositories&q=rudder&type=&language=">
@@ -211,19 +212,19 @@ const App: FunctionComponent = () => (
         </Section>
       </Category>
       <Category>
-        <CategoryTitle>Education</CategoryTitle>
+        <Text type="category">Education</Text>
         <Section>
-          <SectionTitle>University of Waterloo</SectionTitle>
-          <Paragraph>
+          <Text type="section">University of Waterloo</Text>
+          <Text type="paragraph">
             Bachelor of Applied Science in Honours Computer Engineering,
             Co-operative Program
-          </Paragraph>
-          <Paragraph>Graduated June 2018</Paragraph>
-          <Paragraph>
+          </Text>
+          <Text type="paragraph">Graduated June 2018</Text>
+          <Text type="paragraph">
             I learned a lot during my time at the University of Waterloo. As a
             student in the computer engineering program, I learned about the
             hardware and software of modern computers.
-          </Paragraph>
+          </Text>
         </Section>
       </Category>
     </main>
